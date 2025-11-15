5a63628c896405ea05c97ed27bf10f0342b12122
WWelna <WWelna@OccultusTerra.com>
2025-11-15 00:11:43 -0600

    added blowfish, skipjack80, and isaac 32/64

---
+++ src/isaac32.zig
@@ -0,0 +1,194 @@
+// Copyright (C) 2025 William Welna (wwelna@occultusterra.com)
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+const std = @import("std");
+
+const isaac32 = struct {
+    randrsl: [256]u32,
+    mm: [256]u32,
+    randcnt: u32,
+    aa: u32,
+    bb: u32,
+    cc: u32,
+
+    pub fn init() isaac32 {
+        return .{ .randrsl = std.mem.zeroes([256]u32), .mm = std.mem.zeroes([256]u32), .randcnt = 0, .aa = 0, .bb = 0, .cc = 0 };
+    }
+
+    inline fn mix(a: *u32, b: *u32, c: *u32, d: *u32, e: *u32, f: *u32, g: *u32, h: *u32) void {
+        a.* ^= b.* << 11;
+        d.* +%= a.*;
+        b.* +%= c.*;
+        b.* ^= c.* >> 2;
+        e.* +%= b.*;
+        c.* +%= d.*;
+        c.* ^= d.* << 8;
+        f.* +%= c.*;
+        d.* +%= e.*;
+        d.* ^= e.* >> 16;
+        g.* +%= d.*;
+        e.* +%= f.*;
+        e.* ^= f.* << 10;
+        h.* +%= e.*;
+        f.* +%= g.*;
+        f.* ^= g.* >> 4;
+        a.* +%= f.*;
+        g.* +%= h.*;
+        g.* ^= h.* << 8;
+        b.* +%= g.*;
+        h.* +%= a.*;
+        h.* ^= a.* >> 9;
+        c.* +%= h.*;
+        a.* +%= b.*;
+    }
+
+    fn randinit(self: *isaac32, flag: bool) void {
+        var i: usize = 0;
+        var a: u32 = 0x9e3779b9;
+        var b: u32 = 0x9e3779b9;
+        var c: u32 = 0x9e3779b9;
+        var d: u32 = 0x9e3779b9;
+        var e: u32 = 0x9e3779b9;
+        var f: u32 = 0x9e3779b9;
+        var g: u32 = 0x9e3779b9;
+        var h: u32 = 0x9e3779b9;
+
+        for (0..4) |t| {
+            _ = t;
+            isaac32.mix(&a, &b, &c, &d, &e, &f, &g, &h);
+        }
+
+        while (i < 255) : (i += 8) {
+            if (flag) {
+                a +%= self.randrsl[i];
+                b +%= self.randrsl[i + 1];
+                c +%= self.randrsl[i + 2];
+                d +%= self.randrsl[i + 3];
+                e +%= self.randrsl[i + 4];
+                f +%= self.randrsl[i + 5];
+                g +%= self.randrsl[i + 6];
+                h +%= self.randrsl[i + 7];
+            }
+            isaac32.mix(&a, &b, &c, &d, &e, &f, &g, &h);
+            self.mm[i] = a;
+            self.mm[i + 1] = b;
+            self.mm[i + 2] = c;
+            self.mm[i + 3] = d;
+            self.mm[i + 4] = e;
+            self.mm[i + 5] = f;
+            self.mm[i + 6] = g;
+            self.mm[i + 7] = h;
+        }
+
+        if (flag) {
+            i = 0;
+            while (i < 255) : (i += 8) {
+                a +%= self.mm[i];
+                b +%= self.mm[i + 1];
+                c +%= self.mm[i + 2];
+                d +%= self.mm[i + 3];
+                e +%= self.mm[i + 4];
+                f +%= self.mm[i + 5];
+                g +%= self.mm[i + 6];
+                h +%= self.mm[i + 7];
+                isaac32.mix(&a, &b, &c, &d, &e, &f, &g, &h);
+                self.mm[i] = a;
+                self.mm[i + 1] = b;
+                self.mm[i + 2] = c;
+                self.mm[i + 3] = d;
+                self.mm[i + 4] = e;
+                self.mm[i + 5] = f;
+                self.mm[i + 6] = g;
+                self.mm[i + 7] = h;
+            }
+        }
+
+        self.isaac();
+    }
+
+    fn isaac(self: *isaac32) void {
+        var i: u32 = 0;
+        var x: u32 = 0;
+        var y: u32 = 0;
+
+        self.cc +%= 1;
+        self.bb +%= self.cc;
+
+        while (i < 255) : (i += 1) {
+            x = self.mm[i];
+            switch (i % 4) {
+                0 => self.aa ^= (self.aa << 13),
+                1 => self.aa ^= (self.aa >> 6),
+                2 => self.aa ^= (self.aa << 2),
+                3 => self.aa ^= (self.aa >> 16),
+                else => unreachable,
+            }
+            self.aa = self.mm[(i + 128) % 255] +% self.aa;
+            y = self.mm[(x >> 2) % 255] +% self.aa +% self.bb;
+            self.mm[i] = y;
+            self.bb = self.mm[(y >> 10) % 255] +% x;
+            self.randrsl[i] = self.bb;
+        }
+    }
+
+    pub fn u8Seed(self: *isaac32, seed: []const u8, flag: bool) void {
+        var i: u32 = 0;
+        while (i < 255) : (i += 1) {
+            if (i > seed.len - 1) self.randrsl[i] = 0 else self.randrsl[i] = seed[i];
+        }
+        self.randinit(flag);
+    }
+
+    pub fn randSeed(self: *isaac32) [255]u32 {
+        var r: [255]u32 = undefined;
+        for (&self.randrsl, &r) |*x, *y| {
+            y.* = std.crypto.random.int(u32);
+            x.* = y.*;
+        }
+        self.randinit(true);
+        return r;
+    }
+
+    pub fn u8Random(self: *isaac32) u8 {
+        return @as(u8, @intCast(self.u32Random() % std.math.maxInt(u8)));
+    }
+
+    pub fn u16Random(self: *isaac32) u16 {
+        return @as(u16, @intCast(self.u32Random() % std.math.maxInt(u16)));
+    }
+
+    pub fn u32Random(self: *isaac32) u32 {
+        const r = self.randrsl[self.randcnt];
+        self.randcnt += 1;
+        if (self.randcnt > 255) {
+            self.isaac();
+            self.randcnt = 0;
+        }
+        return r;
+    }
+
+    pub fn u64Random(self: *isaac32) u64 {
+        return (@as(u64, self.u32Random()) << 32) | @as(u64, self.u32Random());
+    }
+
+    pub fn asciiRand(self: *isaac32) u8 {
+        return @as(u8, @intCast(self.u32Random() % 95 + 32));
+    }
+};
---
+++ src/isaac64.zig
@@ -0,0 +1,195 @@
+// Copyright (C) 2025 William Welna (wwelna@occultusterra.com)
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// I am not sure if this is 100% accurate as there is a bug in the original isaac64.c.
+// I used https://docs.rs/rand_isaac/latest/rand_isaac/isaac64/struct.Isaac64Rng.html pseudo-code as a reference.
+
+const std = @import("std");
+
+const isaac64 = struct {
+    randrsl: [256]u64,
+    mm: [256]u64,
+    randcnt: u64,
+    aa: u64,
+    bb: u64,
+    cc: u64,
+
+    pub fn init() isaac64 {
+        return .{ .randrsl = std.mem.zeroes([256]u64), .mm = std.mem.zeroes([256]u64), .randcnt = 0, .aa = 0, .bb = 0, .cc = 0 };
+    }
+
+    inline fn mix(a: *u64, b: *u64, c: *u64, d: *u64, e: *u64, f: *u64, g: *u64, h: *u64) void {
+        a.* -%= e.*;
+        f.* ^= h.* >> 9;
+        h.* +%= a.*;
+        b.* -%= f.*;
+        g.* ^= a.* << 9;
+        a.* +%= b.*;
+        c.* -%= g.*;
+        h.* ^= b.* >> 23;
+        b.* +%= c.*;
+        d.* -%= h.*;
+        a.* ^= c.* << 15;
+        c.* +%= d.*;
+        e.* -%= a.*;
+        b.* ^= d.* >> 14;
+        d.* +%= e.*;
+        f.* -%= b.*;
+        c.* ^= e.* << 20;
+        e.* +%= f.*;
+        g.* -%= c.*;
+        d.* ^= f.* >> 17;
+        f.* +%= g.*;
+        h.* -%= d.*;
+        e.* ^= g.* << 14;
+        g.* +%= h.*;
+    }
+
+    fn randinit(self: *isaac64, flag: bool) void {
+        var i: usize = 0;
+        var a: u64 = 0x9e3779b97f4a7c13;
+        var b: u64 = 0x9e3779b97f4a7c13;
+        var c: u64 = 0x9e3779b97f4a7c13;
+        var d: u64 = 0x9e3779b97f4a7c13;
+        var e: u64 = 0x9e3779b97f4a7c13;
+        var f: u64 = 0x9e3779b97f4a7c13;
+        var g: u64 = 0x9e3779b97f4a7c13;
+        var h: u64 = 0x9e3779b97f4a7c13;
+
+        for (0..4) |t| {
+            _ = t;
+            isaac64.mix(&a, &b, &c, &d, &e, &f, &g, &h);
+        }
+
+        while (i < 255) : (i += 8) {
+            if (flag) {
+                a +%= self.randrsl[i];
+                b +%= self.randrsl[i + 1];
+                c +%= self.randrsl[i + 2];
+                d +%= self.randrsl[i + 3];
+                e +%= self.randrsl[i + 4];
+                f +%= self.randrsl[i + 5];
+                g +%= self.randrsl[i + 6];
+                h +%= self.randrsl[i + 7];
+            }
+            isaac64.mix(&a, &b, &c, &d, &e, &f, &g, &h);
+            self.mm[i] = a;
+            self.mm[i + 1] = b;
+            self.mm[i + 2] = c;
+            self.mm[i + 3] = d;
+            self.mm[i + 4] = e;
+            self.mm[i + 5] = f;
+            self.mm[i + 6] = g;
+            self.mm[i + 7] = h;
+        }
+
+        if (flag) {
+            i = 0;
+            while (i < 255) : (i += 8) {
+                a +%= self.mm[i];
+                b +%= self.mm[i + 1];
+                c +%= self.mm[i + 2];
+                d +%= self.mm[i + 3];
+                e +%= self.mm[i + 4];
+                f +%= self.mm[i + 5];
+                g +%= self.mm[i + 6];
+                h +%= self.mm[i + 7];
+                isaac64.mix(&a, &b, &c, &d, &e, &f, &g, &h);
+                self.mm[i] = a;
+                self.mm[i + 1] = b;
+                self.mm[i + 2] = c;
+                self.mm[i + 3] = d;
+                self.mm[i + 4] = e;
+                self.mm[i + 5] = f;
+                self.mm[i + 6] = g;
+                self.mm[i + 7] = h;
+            }
+        }
+
+        self.isaac();
+    }
+
+    inline fn step(self: *isaac64, sugar: u64, i: u64) void {
+        const x: u64 = self.mm[i];
+        self.aa = sugar +% self.mm[(i + 128) % 256];
+
+        self.mm[i] = self.aa +% self.bb +% self.mm[@as(u8, @intCast((x >> 3) % 256))];
+        self.bb = self.mm[@as(u8, @intCast((self.mm[i] >> 11) % 256))] +% x;
+        self.randrsl[i] = self.bb;
+    }
+
+    fn isaac(self: *isaac64) void {
+        var i: u64 = undefined;
+        self.cc +%= 1;
+        self.bb +%= self.cc;
+
+        i = 0;
+        while (i < 256) : (i += 4) {
+            self.step(~(self.aa ^ (self.aa << 21)), i);
+            self.step(self.aa ^ (self.aa >> 5), i + 1);
+            self.step(self.aa ^ (self.aa << 12), i + 2);
+            self.step(self.aa ^ (self.aa >> 33), i + 3);
+        }
+    }
+
+    pub fn u8Seed(self: *isaac64, seed: []const u8, flag: bool) void {
+        var i: u64 = 0;
+        while (i < 255) : (i += 1) {
+            if (i > seed.len - 1) self.randrsl[i] = 0 else self.randrsl[i] = seed[i];
+        }
+        self.randinit(flag);
+    }
+
+    pub fn randSeed(self: *isaac64) [255]u64 {
+        var r: [255]u64 = undefined;
+        for (&self.randrsl, &r) |*x, *y| {
+            y.* = std.crypto.random.int(u64);
+            x.* = y.*;
+        }
+        self.randinit(true);
+        return r;
+    }
+
+    pub fn u8Random(self: *isaac64) u8 {
+        return @as(u8, @intCast(self.u64Random() % std.math.maxInt(u8)));
+    }
+
+    pub fn u16Random(self: *isaac64) u16 {
+        return @as(u16, @intCast(self.u64Random() % std.math.maxInt(u16)));
+    }
+
+    pub fn u32Random(self: *isaac64) u32 {
+        return @as(u32, @intCast(self.u64Random() % std.math.maxInt(u32)));
+    }
+
+    pub fn u64Random(self: *isaac64) u64 {
+        const r = self.randrsl[self.randcnt];
+        self.randcnt += 1;
+        if (self.randcnt > 255) {
+            self.isaac();
+            self.randcnt = 0;
+        }
+        return r;
+    }
+
+    pub fn asciiRand(self: *isaac64) u8 {
+        return @as(u8, @intCast(self.u64Random() % 95 + 32));
+    }
+};
